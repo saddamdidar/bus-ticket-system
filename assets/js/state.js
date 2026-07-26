274fa60f1a7e6096c8d3f807e0a617873aeac1e9
saddamdidar <hkl.saddam@gmail.com>
2026-07-26 20:08:54 +0600

    Create state.js

---
+++ assets/js/state.js
@@ -0,0 +1,9 @@
+      // ============ APPLICATION STATE ============
+    
+    const APP_STATE = {
+    selectedCoach: "",
+    selectedDate: "",
+    selectedSeats: [],
+    selectedPayment: "",
+    currentStep: 1
+};
