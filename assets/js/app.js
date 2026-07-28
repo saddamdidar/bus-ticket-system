50b034ebc0dbaa863e85380ee1081f9d33721a51
saddamdidar <hkl.saddam@gmail.com>
2026-07-28 19:25:29 +0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,18 @@
+// ===============================
+// GLOBAL APPLICATION STATE
+// ===============================
+
+const APP_STATE = {
+    selectedCoach: "",
+    selectedDate: "",
+    selectedSeats: [],
+    passengers: [],
+    selectedPayment: "",
+    currentStep: 1
+};
+
+
+
 function initializeApp() {
 
     console.log("Initializing App...");
